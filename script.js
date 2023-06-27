75c3ead1775e668a74525b217ad1de557e16ce1e
Marcin <mkula.poczta@gmail.com>
2023-06-27 14:45:53 +0200

    swap functionality added

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ const swapBtn = document.querySelector(".swap")
 const rateInfo = document.querySelector(".rate-info")
 
 const calculate = () => {
+	const currency1 = currencyOne.value
+	const currency2 = currencyTwo.value
+
 	fetch(
-		`https://api.exchangerate.host/convert?from=${currencyOne.value}&to=${currencyTwo.value}`
+		`https://api.exchangerate.host/convert?from=${currency1}&to=${currency2}`
 	)
 		.then(res => res.json())
 		.then(data => {
-			console.log(data)
-			console.log(data.info)
-			console.log(currencyOne.value)
-			const currency1 = currencyOne.value
-			const currency2 = currencyTwo.value
-			rateInfo.textContent = `1 ${currency1} = ${data.result.toFixed(
+			// console.log(data)
+			// console.log(data.info)
+			// console.log(currencyOne.value)
+
+			const exRate = data.result
+			rateInfo.textContent = `1 ${currency1} = ${exRate.toFixed(
 				4
 			)} ${currency2}`
 
@@ -25,8 +28,18 @@ const calculate = () => {
 		})
 }
 
+const swap = () => {
+	const currency1 = currencyOne.value
+	const currency2 = currencyTwo.value
+
+	currencyOne.value = currency2
+	currencyTwo.value = currency1
+
+	calculate()
+}
+
 currencyOne.addEventListener("change", calculate)
 currencyTwo.addEventListener("change", calculate)
 amountOne.addEventListener("input", calculate)
-
+swapBtn.addEventListener("click", swap)
 calculate()
