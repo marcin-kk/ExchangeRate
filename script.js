f81156eca5c897fae224b3d8f1b4400b9681391c
Marcin <mkula.poczta@gmail.com>
2023-06-27 11:23:30 +0200

    JS - culculation functionality in place, listeners added on inputs, and selects

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ const swapBtn = document.querySelector(".swap")
 const rateInfo = document.querySelector(".rate-info")
 
 const calculate = () => {
-	fetch("https://api.exchangerate.host/convert?from=USD&to=EUR")
+	fetch(
+		`https://api.exchangerate.host/convert?from=${currencyOne.value}&to=${currencyTwo.value}`
+	)
 		.then(res => res.json())
 		.then(data => {
-            console.log(data)
-            console.log(data.info)
-        
-        
-        })
+			console.log(data)
+			console.log(data.info)
+			console.log(currencyOne.value)
+			const currency1 = currencyOne.value
+			const currency2 = currencyTwo.value
+			rateInfo.textContent = `1 ${currency1} = ${data.result.toFixed(
+				4
+			)} ${currency2}`
+
+			const calcCurrency = amountOne.value * data.result
+			amountTwo.value = calcCurrency.toFixed(2)
+		})
 }
+
+currencyOne.addEventListener("change", calculate)
+currencyTwo.addEventListener("change", calculate)
+amountOne.addEventListener("input", calculate)
 
 calculate()
